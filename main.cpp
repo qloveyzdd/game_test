862e8f365f310454a5c31f7ffad58c3ae91050fa
qlovey <84934701@qq.com>
2021-07-04 05:56:10 +0800

    调试修改bug

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-#include"Creature.h"
-#include"Adventure.h"
+#include "Creature.h"
+#include "Adventure.h"
 
 int main()
 {
-    std::cout<<"hello world!!"<<std::endl;
+    int d = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        d += i;
+    }
+
+    std::cout << "hello world!!" << d << std::endl;
     return 0;
 }
